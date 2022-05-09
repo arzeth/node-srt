f28a9eccf6058f2373cc7d3def71724233f2fc2a
Stephan Hesse <stephan@emliri.com>
2022-05-09 20:49:07 +0200

    examples: client-server clarify and proper err handling

--- examples/srt-client-server.ts
+++ examples/srt-client-server.ts
@@ -12,9 +12,17 @@ let recvBytes = 0
 let sentBytes = 0;
 
 new Promise<void>(async (resolve, reject) => {
-  const srtServer: SRTServer = new SRTServer(8002, '127.0.0.1');
 
-  srtServer.on('connection', async (connection: SRTServerConnection) => {
+  const srtServer: SRTServer = new SRTServer(8002, '127.0.0.1');
+  srtServer.on('connection', onConnectionServer);
+  await srtServer.create()
+  await srtServer.open();
+
+  const srtClient = new SRTClientConnection(8002, '127.0.0.1');
+  await srtClient.create();
+  await srtClient.open();
+
+  async function onConnectionServer(connection: SRTServerConnection) {
     console.log('server-connection fd:', connection.fd);
 
     while(true) {
@@ -22,29 +30,25 @@ new Promise<void>(async (resolve, reject) => {
       const pkt = new Uint8Array(1000);
       sentBytes += pkt.byteLength;
       const res = await connection.write(pkt);
-      if (res !== SRTResult.SRT_OK) {
-        throw new Error();
+      if (res === SRTResult.SRT_ERROR) {
+        throw new Error('Server write failed: ' + srtServer.asyncSrt.getError()?.message);
       }
       console.log('server sent bytes:', sentBytes);
 
-      const result = await srtClient.read(16 * 1024);
-      if (result instanceof Uint8Array) {
-        recvBytes += result.byteLength;
-        console.log('client read bytes:', recvBytes, result.byteLength)
-      } else {
-        console.log('client read error:', result)
-      }
-
+      srtClient.read(16 * 1024)
+        .then(onReadClient)
+        .catch(err => { throw new Error(err)});
     }
+  }
 
-  });
-
-  await srtServer.create()
-  await srtServer.open();
-
-  const srtClient = new SRTClientConnection(8002, '127.0.0.1');
-
-  await srtClient.create();
-  await srtClient.open();
+  function onReadClient(result: Uint8Array | SRTResult.SRT_ERROR) {
+    if (result instanceof Uint8Array) {
+      recvBytes += result.byteLength;
+      console.log('client read bytes:', recvBytes, result.byteLength)
+      console.log('client-server diff:', sentBytes - recvBytes);
+    } else {
+      console.log('client read error')
+    }
+  }
 
 });
