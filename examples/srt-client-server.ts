f7a8c06c6d8cc6f82c692674e1b0edade98e087c
Stephan Hesse <stephan@emliri.com>
2022-05-09 19:44:31 +0200

    move client-server testbench to examples

--- examples/srt-client-server.ts
+++ examples/srt-client-server.ts
@@ -1,4 +1,7 @@
+#!/usr/bin/env ts-node
+
 import { SRT, SRTClientConnection, SRTServer } from '../';
+import { SRTResult } from '../src/srt-api-enums';
 import { SRTServerConnection } from '../types/srt-server';
 
 new SRT().setLogLevel(7);
@@ -8,8 +11,6 @@ let char = 0;
 let recvBytes = 0
 let sentBytes = 0;
 
-
-
 new Promise<void>(async (resolve, reject) => {
   const srtServer: SRTServer = new SRTServer(8002, '127.0.0.1');
 
@@ -17,18 +18,23 @@ new Promise<void>(async (resolve, reject) => {
     console.log('server-connection fd:', connection.fd);
 
     while(true) {
+
       const pkt = new Uint8Array(1000);
-      await connection.write(pkt);
       sentBytes += pkt.byteLength;
+      const res = await connection.write(pkt);
+      if (res !== SRTResult.SRT_OK) {
+        throw new Error();
+      }
       console.log('server sent bytes:', sentBytes);
-      srtClient.read(16 * 1024).then(result => {
-        if (result instanceof Uint8Array) {
-          recvBytes += result.byteLength;
-          console.log('client read bytes:', result.byteLength, recvBytes)
-        } else {
-          console.log('client read error:', result)
-        }
-      });
+
+      const result = await srtClient.read(16 * 1024);
+      if (result instanceof Uint8Array) {
+        recvBytes += result.byteLength;
+        console.log('client read bytes:', recvBytes, result.byteLength)
+      } else {
+        console.log('client read error:', result)
+      }
+
     }
 
   });
