d2d0253f8063884d14281079171f09b4d8df4618
Stephan Hesse <stephan@emliri.com>
2022-05-25 09:58:03 +0200

    worker-provider: declare expl ASYNC_WORKER_PATH module const

--- src/async-worker-provider.js
+++ src/async-worker-provider.js
@@ -1,7 +1,9 @@
 const { Worker } = require('worker_threads');
 const { resolve } = require('path');
 
-const getAsyncWorkerPath = () => resolve(__dirname, './async-worker.js');
+const ASYNC_WORKER_PATH = './async-worker.js';
+
+const getAsyncWorkerPath = () => resolve(__dirname, ASYNC_WORKER_PATH);
 
 let _createAsyncWorker = () => {
   return new Worker(getAsyncWorkerPath());
